14195f6be97ca8b751a78146d50d0629430ca8e3
Sabin Thapa <sabint017@gmail.com>
2023-04-19 13:41:13 +0545

    feat: init new crash course project

---
+++ express-crash-course-traversy-media/data/Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        name: "Sabin Thapa",
+        email: "sabin@gmail.com",
+        status: "active"
+    },
+    {
+        name: "Image Thapa",
+        email: "image@gmail.com",
+        status: "inactive"
+    },
+    {
+        name: "Maskey Thapa",
+        email: "maskey@gmail.com",
+        status: "active"
+    },
+    {
+        name: "Sabin Maskey",
+        email: "sm @gmail.com",
+        status: "inactive"
+    },
+]
+
+module.exports = users

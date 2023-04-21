1f835e043473bdc43a5538fd8284fd43e80043ba
Sabin Thapa <sabint017@gmail.com>
2023-04-21 14:29:56 +0545

    fix: send user id on successful authorization

--- day5/jwt-auth/routes/posts.js
+++ day5/jwt-auth/routes/posts.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const verifier = require('../verifyToken')
 
 router.get('/', verifier, (req, res) => {
-    res.json({posts: {title: "POST", desc: "RANDOM DATA"}})
+    res.send(req.user)
 })
 
 module.exports = router

b4b69b5e7b6d06ac43e29196f82c83b1b3b53bed
Sabin Thapa <sabint017@gmail.com>
2023-04-21 13:46:50 +0545

    feat: connect to MongoDB + User validation

--- day5/jwt-auth/index.js
+++ day5/jwt-auth/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+
+//dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+//Middleware
+app.use(express.json())
+
 //Import Routes
 const authRoute = require('./routes/auth')
 
@@ -10,6 +20,11 @@ app.use('/api/user', authRoute)
 
 const PORT = 3000
 
+// DB Connection
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true})
+    .then(console.log('DB Connected!'))
+    .catch(err => console.error(err.message))
+
 app.listen(PORT, () => {
-    console.log(`Server running on PORt ${PORT}`);
+    console.log(`Server running on PORT ${PORT}`);
 })
--- day5/jwt-auth/routes/auth.js
+++ day5/jwt-auth/routes/auth.js
@@ -1,8 +1,45 @@
 const router = require('express').Router()
+const User = require('../models/User')
 
-router.post('/register', (req, res) => {
-    res.send('Register')
-})
+
+//VALIDATION
+const joi = require('@hapi/joi')
+
+const validationSchema = {
+    name: joi.string().min(6).required(),
+    email: joi.string().email().min(6).required(),
+    password: joi.string().min(6).required()
+}
+
+router.post('/register', async (req, res) => {
+
+    //VALIDATE DATA BEFORE POSTING
+    const {error} = joi.validate(req.body, validationSchema)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const {name, email, password} = req.body
+    const user = new User({
+        name, email, password
+    })
+    try {
+        const savedUser = await user.save()
+        console.log(savedUser, 'saved')
+        res.send(savedUser)
+    } catch(err) {
+        res.status(400).send({
+            msg: err.message
+        })
+    }
+   })
+
+// router.get('/register', async(req, res) => {
+//     try {
+//         const users = await User.find({})
+//         res.send(users)
+//     } catch (error) {
+//         res.status(400).send({msg: error.message})
+//     }
+// })
 
 router.post('/login', (req, res) => {
     res.send('Login')

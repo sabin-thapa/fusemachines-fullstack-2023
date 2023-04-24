af809b2033b2a5cc2b45c7bfea7a920d533215c9
Sabin Thapa <sabint017@gmail.com>
2023-04-24 15:03:22 +0545

    feat: setup frontend and backend and mongodb

--- day8/auth-project/server/index.js
+++ day8/auth-project/server/index.js
@@ -1,20 +1,61 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+const bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({extended: true}))
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require("mongoose");
+const User = require('./models/User')
 
 // CORS
-const cors = require('cors')
-app.use(cors())
-app.use(express.json())
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 
-const PORT = 1337
+const PORT = 1337;
 
-app.post('/api/register', (req, res) => {
-    console.log(req.body, "BODY")
-    res.json({
-        status: "ok"
+app.post("/api/register", async (req, res) => {
+  try {
+    console.log(req.body)
+    const user = await User.create({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
     })
-})
+    res.json({
+      status: "ok",
+      data: user
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message, type: 'error' });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const user = User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    })
+    res.json({
+      status: "ok",
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+//Mongoose conn
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.ADMIN_PW}@internship.ddm4lh4.mongodb.net/mern-auth?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("DB Connected!"));
 
 app.listen(PORT, () => {
-    console.log('Server started at port:', PORT)
-})
+  console.log("Server started at port:", PORT);
+});

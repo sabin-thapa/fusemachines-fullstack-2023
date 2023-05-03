0b60958738d6444622520c107d8ed1b39d8482f1
Sabin Thapa <sabint017@gmail.com>
2023-05-03 13:28:00 +0545

    feat: add question count

--- challenges/quiz-with-timer/script.js
+++ challenges/quiz-with-timer/script.js
@@ -3,13 +3,26 @@ const nextBtn = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-btns");
+const questionCountElement = document.getElementById('question-count')
+
 
 startBtn.addEventListener("click", startQuiz);
 
 let shuffledQuestions, currentQuestionIndex;
 
+const containerElement = document.getElementById('container')
+containerElement.style.boxShadow = 'none';
+containerElement.style.backgroundColor = 'hsl(145, 100%, 25%)';
+
+nextBtn.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
 function startQuiz() {
   console.log("Started");
+  containerElement.style.boxShadow = "0 0 10px 2px";
+  containerElement.style.backgroundColor = '#fff';
   startBtn.classList.add("hide");
   questionContainerElement.classList.remove("hide");
   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
@@ -23,6 +36,7 @@ function setNextQuestion() {
 }
 
 function showQuestion(question) {
+  questionCountElement.innerText = `Question ${currentQuestionIndex+1} of ${questions.length}`;
   questionElement.innerText = question.question;
   question.answers.forEach((answer) => {
     const button = document.createElement("button");
@@ -43,9 +57,17 @@ function selectAnswer(e) {
   Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
   });
+
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextBtn.classList.remove("hide");
+  } else {
+    startBtn.innerText = "Restart Quiz";
+    startBtn.classList.remove("hide");
+  }
 }
 
 function clearState() {
+  clearStatusClass(document.body);
   nextBtn.classList.add("hide");
 
   //Remove duplicate children
@@ -74,6 +96,42 @@ const questions = [
     answers: [
       { text: "Object Oriented Programming", correct: true },
       { text: "Object O Programming", correct: false },
+      { text: "Object Oriented P", correct: false },
+      { text: "Object OO PP", correct: false },
+    ],
+  },
+  {
+    question: "What is the full form of HTML?",
+    answers: [
+      { text: "Hyper Text Markup Language", correct: true },
+      { text: "Object O Programming", correct: false },
+      { text: "Object Oriented P", correct: false },
+      { text: "Object OO PP", correct: false },
+    ],
+  },
+  {
+    question: "What is the full form of CSS?",
+    answers: [
+      { text: "Object Oriented Programming", correct: true },
+      { text: "Object O Programming", correct: false },
+      { text: "Cascading Style Sheet", correct: false },
+      { text: "Object OO PP", correct: false },
+    ],
+  },
+  {
+    question: "What is the full form of FTP?",
+    answers: [
+      { text: "Object Oriented Programming", correct: false },
+      { text: "Object O Programming", correct: false },
+      { text: "Object Oriented P", correct: false },
+      { text: "File Transfer Protocol", correct: true },
+    ],
+  },
+  {
+    question: "What is the full form of XML?",
+    answers: [
+      { text: "Object Oriented Programming", correct: false },
+      { text: "Extensible Markup Language", correct: true },
       { text: "Object Oriented P", correct: false },
       { text: "Object OO PP", correct: false },
     ],

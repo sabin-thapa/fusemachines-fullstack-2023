bb61417786e84752118a4056e9c787cd63fc7ef5
Sabin Thapa <sabint017@gmail.com>
2023-05-04 09:52:36 +0545

    chore: move questions to a different file

--- challenges/quiz-with-timer/questions.js
+++ challenges/quiz-with-timer/questions.js
@@ -1,71 +1,79 @@
-// creating an array and passing the number, questions, options, and answers
-let questions = [
-    {
-    numb: 1,
+export const questions = [
+  {
     question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
+    answers: [
+      { text: "Hyper Text Markup Language", correct: true },
+      { text: "Hyperlinks and Text Markup Language", correct: false },
+      { text: "Home Tool Markup Language", correct: false },
+      { text: "Hyperlink Tool Markup Language", correct: false },
+    ],
   },
-    {
-    numb: 2,
+  {
     question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
+    answers: [
+      { text: "Cascading Style Sheets", correct: true },
+      { text: "Cascading Style System", correct: false },
+      { text: "Cascading Sheet Styles", correct: false },
+      { text: "Cascading Style Syntax", correct: false },
+    ],
   },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
+  {
+    question: "What does JS stand for?",
+    answers: [
+      { text: "JavaScript", correct: true },
+      { text: "JavaSyntax", correct: false },
+      { text: "JellyScript", correct: false },
+      { text: "JavaStyle", correct: false },
+    ],
   },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
+  {
+    question: "What is the purpose of HTML?",
+    answers: [
+      { text: "To define the structure of web pages", correct: true },
+      { text: "To style web pages", correct: false },
+      { text: "To add interactivity to web pages", correct: false },
+      { text: "To communicate with databases", correct: false },
+    ],
   },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
+  {
+    question: "What is the purpose of CSS?",
+    answers: [
+      { text: "To style web pages", correct: true },
+      { text: "To define the structure of web pages", correct: false },
+      { text: "To add interactivity to web pages", correct: false },
+      { text: "To communicate with databases", correct: false },
+    ],
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+    question: "What is the purpose of JavaScript?",
+    answers: [
+      { text: "To add interactivity to web pages", correct: true },
+      { text: "To define the structure of web pages", correct: false },
+      { text: "To style web pages", correct: false },
+      { text: "To communicate with databases", correct: false },
+    ],
+  },
+  {
+    question: "What is the correct syntax for an HTML comment?",
+    answers: [
+      { text: "<!-- This is a comment -->", correct: true },
+      { text: "// This is a comment", correct: false },
+      { text: "/* This is a comment */", correct: false },
+      { text: "<comment>This is a comment</comment>", correct: false },
+    ],
+  },
+  {
+    question: "What is the correct syntax for an external JS file?",
+    answers: [
+      { text: "<script src='myscript.js'></script>", correct: true },
+      {
+        text: "<link rel='script' type='text/js' href='myscript.js'>",
+        correct: false,
+      },
+      {
+        text: "<head> <script src='myscript.js'></script> </head>",
+        correct: false,
+      },
+    ],
+  },
 ];
--- challenges/quiz-with-timer/script.js
+++ challenges/quiz-with-timer/script.js
@@ -1,3 +1,5 @@
+import {questions} from './questions.js' 
+
 const startBtn = document.getElementById("start-btn");
 const instructionBtn = document.getElementById("instruction-btn");
 const aboutBtn = document.getElementById("about-btn");
@@ -176,83 +178,3 @@ function checkAnswer(answer, correct) {
 function showScore() {
   scoreElement.innerText = score;
 }
-
-const questions = [
-  {
-    question: "What does HTML stand for?",
-    answers: [
-      { text: "Hyper Text Markup Language", correct: true },
-      { text: "Hyperlinks and Text Markup Language", correct: false },
-      { text: "Home Tool Markup Language", correct: false },
-      { text: "Hyperlink Tool Markup Language", correct: false },
-    ],
-  },
-  {
-    question: "What does CSS stand for?",
-    answers: [
-      { text: "Cascading Style Sheets", correct: true },
-      { text: "Cascading Style System", correct: false },
-      { text: "Cascading Sheet Styles", correct: false },
-      { text: "Cascading Style Syntax", correct: false },
-    ],
-  },
-  {
-    question: "What does JS stand for?",
-    answers: [
-      { text: "JavaScript", correct: true },
-      { text: "JavaSyntax", correct: false },
-      { text: "JellyScript", correct: false },
-      { text: "JavaStyle", correct: false },
-    ],
-  },
-  {
-    question: "What is the purpose of HTML?",
-    answers: [
-      { text: "To define the structure of web pages", correct: true },
-      { text: "To style web pages", correct: false },
-      { text: "To add interactivity to web pages", correct: false },
-      { text: "To communicate with databases", correct: false },
-    ],
-  },
-  {
-    question: "What is the purpose of CSS?",
-    answers: [
-      { text: "To style web pages", correct: true },
-      { text: "To define the structure of web pages", correct: false },
-      { text: "To add interactivity to web pages", correct: false },
-      { text: "To communicate with databases", correct: false },
-    ],
-  },
-  {
-    question: "What is the purpose of JavaScript?",
-    answers: [
-      { text: "To add interactivity to web pages", correct: true },
-      { text: "To define the structure of web pages", correct: false },
-      { text: "To style web pages", correct: false },
-      { text: "To communicate with databases", correct: false },
-    ],
-  },
-  {
-    question: "What is the correct syntax for an HTML comment?",
-    answers: [
-      { text: "<!-- This is a comment -->", correct: true },
-      { text: "// This is a comment", correct: false },
-      { text: "/* This is a comment */", correct: false },
-      { text: "<comment>This is a comment</comment>", correct: false },
-    ],
-  },
-  {
-    question: "What is the correct syntax for an external JS file?",
-    answers: [
-      { text: "<script src='myscript.js'></script>", correct: true },
-      {
-        text: "<link rel='script' type='text/js' href='myscript.js'>",
-        correct: false,
-      },
-      {
-        text: "<head> <script src='myscript.js'></script> </head>",
-        correct: false,
-      },
-    ],
-  },
-];
